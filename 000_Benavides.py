d13793ac97e09a658a9d0694fbffe0d43fb69b77
Cherybv <141943281+Cherybv@users.noreply.github.com>
2023-08-28 21:28:57 -0600

    000_Benavides.py
    Modus Ponens

---
+++ 000_Benavides.py
@@ -0,0 +1,9 @@
+# Afirmaciones 
+a1 = True # Si el alumno entrega la tarea, el alunmo tendra los puntos
+a2 = True # El alumno entrego la tarea
+
+# Modus Ponens
+if a2:
+  if a1:
+    c = "El alumno tiene los puntos"
+    print(c)
