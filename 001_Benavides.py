b88f36ed81b723dff5a8d792704fb4c389333c91
Cherybv <141943281+Cherybv@users.noreply.github.com>
2023-08-28 21:35:44 -0600

    001_Benavides.py
    Modus Tollens

---
+++ 001_Benavides.py
@@ -0,0 +1,8 @@
+# Afirmaciones
+a1 = True # Si la bocina no tiene bateria, entonces la usaron
+a2 = False # La bocina tiene bateria
+
+# Modus tollens
+if not a1:
+  c = No usaron la bocina
+  print(c)
